aa987c466fbaad9583669693faaff28ff4b7485b
MarcosGueva <ggusnay@est.ups.edu.ec>
2025-07-04 14:15:54 -0500

    Update
    Correción de carpeta en controller y actualización de la carpeta main

--- caja_ahorros_api/main.py
+++ caja_ahorros_api/main.py
@@ -1,6 +1,26 @@
 from fastapi import FastAPI
 from controllers.auth_controller import router as auth_router
+from controllers.ahorro_controller import router as ahorro_router
+from controllers.credito_controller import router as credito_router
+from controllers.reporte_controller import router as reporte_router
+from controllers.auditoria_controller import router as auditoria_router
+from controllers.socio_controller import router as socio_router
+from controllers.ingreso_egreso_controller import router as ingreso_egreso_router
+from controllers.reporte_ingreso_egreso_controller import router as reporte_ingreso_egreso_router
+
+
+
+
 
 app = FastAPI(title="API Caja de Ahorros")
 
 app.include_router(auth_router, prefix="/auth", tags=["Autenticación"])
+app.include_router(ahorro_router, prefix="/ahorros", tags=["Ahorros"])
+app.include_router(credito_router, prefix="/creditos", tags=["Créditos"])
+app.include_router(reporte_router, prefix="/reportes", tags=["Reportes"])
+app.include_router(auditoria_router, prefix="/auditoria", tags=["Auditoría"])
+app.include_router(socio_router, prefix="/socios", tags=["Socios"])
+app.include_router(ingreso_egreso_router, prefix="/ingresos-egresos", tags=["Ingresos y Egresos"])
+app.include_router(reporte_ingreso_egreso_router, prefix="/reportes-ingresos-egresos", tags=["Reportes Ingresos y Egresos"])
+
+
